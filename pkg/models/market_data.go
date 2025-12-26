41705066ba15825dd067b65cf34a7439a23fae57
Lev <tolkoplus@gmail.com>
2025-12-26 03:25:26 +0300

    mexc add map lastmarket  in struct

--- pkg/models/market_data.go
+++ pkg/models/market_data.go
@@ -6,5 +6,7 @@ type MarketData struct {
 	Symbol    string  `json:"symbol"`    // торговая пара, например BTC-USDT
 	Bid       float64 `json:"bid"`       // лучшая цена покупки
 	Ask       float64 `json:"ask"`       // лучшая цена продажи
+	BidSize   float64 `json:"bid_size"`  // объём на лучшей цене покупки
+	AskSize   float64 `json:"ask_size"`  // объём на лучшей цене продажи
 	Timestamp int64   `json:"timestamp"` // метка времени в миллисекундах
 }
