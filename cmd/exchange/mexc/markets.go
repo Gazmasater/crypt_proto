330468351fa85df320dc5e8199f404e61c53ce34
Lev <tolkoplus@gmail.com>
2026-01-03 12:25:16 +0300

    triangle  kucoin+mexc  new arh

--- cmd/exchange/mexc/markets.go
+++ cmd/exchange/mexc/markets.go
@@ -3,21 +3,28 @@ package mexc
 import (
 	"crypt_proto/cmd/exchange/common"
 	"encoding/json"
-	"io"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 type mexcSymbol struct {
-	Symbol               string `json:"symbol"`
-	BaseAsset            string `json:"baseAsset"`
-	QuoteAsset           string `json:"quoteAsset"`
-	Status               string `json:"status"`
-	BaseSizePrecision    int    `json:"baseSizePrecision"`
-	QuotePrecision       int    `json:"quotePrecision"`
-	MinQty               string `json:"minQty"`
-	IsSpotTradingAllowed bool   `json:"isSpotTradingAllowed"`
+	Symbol     string `json:"symbol"`
+	BaseAsset  string `json:"baseAsset"`
+	QuoteAsset string `json:"quoteAsset"`
+	Status     string `json:"status"`
+
+	IsSpotTradingAllowed bool `json:"isSpotTradingAllowed"`
+
+	BaseAssetPrecision  int `json:"baseAssetPrecision"`
+	QuoteAssetPrecision int `json:"quoteAssetPrecision"`
+
+	Filters []struct {
+		FilterType string `json:"filterType"`
+		MinQty     string `json:"minQty"`
+		StepSize   string `json:"stepSize"`
+	} `json:"filters"`
 }
 
 type mexcResponse struct {
@@ -31,48 +38,41 @@ func LoadMarkets() map[string]common.Market {
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
-
 	var api mexcResponse
-	if err := json.Unmarshal(body, &api); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&api); err != nil {
 		log.Fatalf("decode error: %v", err)
 	}
 
 	markets := make(map[string]common.Market)
+	fmt.Println("STATUS", api.Data)
 
 	for _, s := range api.Data {
+		fmt.Println("STATUS", s.Symbol)
 		if s.Status != "ENABLED" || !s.IsSpotTradingAllowed {
 			continue
 		}
 
-		minQty, _ := strconv.ParseFloat(s.MinQty, 64)
+		var minQty, stepSize float64
 
-		baseStep := precisionToStep(s.BaseSizePrecision)
-		quoteStep := precisionToStep(s.QuotePrecision)
+		for _, f := range s.Filters {
+			if f.FilterType == "LOT_SIZE" {
+				minQty, _ = strconv.ParseFloat(f.MinQty, 64)
+				stepSize, _ = strconv.ParseFloat(f.StepSize, 64)
+			}
+		}
 
 		key := s.BaseAsset + "_" + s.QuoteAsset
 
 		markets[key] = common.Market{
-			Symbol:         s.Symbol,
-			Base:           s.BaseAsset,
-			Quote:          s.QuoteAsset,
-			EnableTrading:  true,
-			BaseMinSize:    minQty,
-			BaseIncrement:  baseStep,
-			QuoteIncrement: quoteStep,
+			Symbol:        s.Symbol,
+			Base:          s.BaseAsset,
+			Quote:         s.QuoteAsset,
+			EnableTrading: true,
+
+			BaseMinSize:   minQty,
+			BaseIncrement: stepSize,
 		}
 	}
 
 	return markets
-}
-
-func precisionToStep(p int) float64 {
-	if p <= 0 {
-		return 1
-	}
-	step := 1.0
-	for i := 0; i < p; i++ {
-		step /= 10
-	}
-	return step
 }
