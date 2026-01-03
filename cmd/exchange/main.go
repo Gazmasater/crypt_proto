4176e5c086ea36acbc067189e7ad95a820a4697e
Lev <tolkoplus@gmail.com>
2026-01-03 12:10:41 +0300

    triangle  kucoin+mexc  new arh

--- cmd/exchange/main.go
+++ cmd/exchange/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"log"
+
 	"crypt_proto/cmd/exchange/builder"
 	"crypt_proto/cmd/exchange/common"
 	"crypt_proto/cmd/exchange/kucoin"
+	"crypt_proto/cmd/exchange/mexc"
 )
 
 func main() {
-	markets := kucoin.LoadMarkets()
 
-	triangles := builder.BuildTriangles(markets, "USDT")
+	// ---------- KUCOIN ----------
+	kucoinMarkets := kucoin.LoadMarkets()
 
-	common.SaveTrianglesCSV("data/kucoin_triangles.csv", triangles)
+	kucoinTriangles := builder.BuildTriangles(kucoinMarkets, "USDT")
+
+	if err := common.SaveTrianglesCSV(
+		"data/kucoin_triangles_usdt.csv",
+		kucoinTriangles,
+	); err != nil {
+		log.Fatalf("kucoin csv error: %v", err)
+	}
+
+	// ---------- MEXC ----------
+	mexcMarkets := mexc.LoadMarkets()
+
+	mexcTriangles := builder.BuildTriangles(mexcMarkets, "USDT")
+
+	if err := common.SaveTrianglesCSV(
+		"data/mexc_triangles_usdt.csv",
+		mexcTriangles,
+	); err != nil {
+		log.Fatalf("mexc csv error: %v", err)
+	}
 }
